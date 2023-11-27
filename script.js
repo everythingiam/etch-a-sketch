ef80b1e42c537d5b04945fc7ea754f800c505dd6
Alina <alina.mikhaylova.03@gmail.com>
2023-11-28 01:33:33 +0300

    Работает ползунок количества пикселей в поле, также пофикшен небольшой баг с рисованием

--- script.js
+++ script.js
@@ -1,34 +1,69 @@
 const field = document.querySelector('.field');
-
+const body = document.querySelector('body');
 var pressed = false;
 
 field.addEventListener('mousedown', (event) => {
     event.preventDefault(); 
     pressed = true;
 });
-field.addEventListener('mouseup', () => {
+body.addEventListener('mouseup', () => {
     pressed = false;
 })
 field.addEventListener('click', (e) => {
     e.target.style.backgroundColor = 'black';
 })
+field.addEventListener('mousemove', (e) => {
+    if (pressed) e.target.style.backgroundColor = 'black';
+})
 
 function fillField(size){
-    let square = size*size;
     let width = field.offsetWidth;
     let pixelSize = width/size;
-    for (let i = 0; i < square; i++){
+    for (let i = 0; i < size*size; i++){
         const pixel = document.createElement('div');
+        pixel.className = 'pixel';
         pixel.setAttribute('style', `width: ${pixelSize}px; height: ${pixelSize}px`);
 
         field.append(pixel);
     }
-    
 }
-fillField(32);
 
+function removeField(){
+    document.querySelectorAll('.pixel').forEach(elem => elem.remove());    
+}
 
+const range = document.querySelector(".range");
+const setting = document.querySelector(".setting");
 
-field.addEventListener('mousemove', (e) => {
-    if (pressed) e.target.style.backgroundColor = 'black';
+range.addEventListener("input", () => {
+    setSetting();
+});
+range.addEventListener("mouseup", () =>{
+    removeField();
+    fillField(setSetting());
 })
+
+function setSetting() {
+    const val = range.value; 
+
+    const min = range.min; 
+    const max = range.max; 
+
+    const steps = (val - min) / (max - min); //процент шага относительно текущего положения (val)
+
+    setting.textContent = val + 'x' + val;
+
+    const rangeWidth = range.offsetWidth;
+    const settingWidth = setting.offsetWidth;
+    const offsetLeft = steps * (rangeWidth - settingWidth); //умножаю процент шага на количество пикселей внутри 
+                                                            //существующего поля (еще вычла ширину самого ползунка
+                                                            //с названием setting. Таким образом считается количество
+                                                            //пикселей, на который должен сместиться setting от левого 
+                                                            //края, в зависимости от ширины поля и текущего значения.)
+
+    setting.style.left = `${offsetLeft}px`; //смсещение
+
+    return val;
+}
+
+fillField(setSetting());
